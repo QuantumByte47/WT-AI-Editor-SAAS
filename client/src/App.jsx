68b76daacf194640aa335a1021ae815c31ad6f4f
Varun Yadav <yadav.varun056038@gmail.com>
2025-07-26 22:01:03 +0530

    Article Generation Feature completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,10 +9,12 @@ import RemoveBackground from "./pages/RemoveBackground";
 import RemoveObject from "./pages/RemoveObject";
 import ReviewResume from "./pages/ReviewResume";
 import Community from "./pages/Community";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <div>
+      <Toaster position="top-center" reverseOrder={false} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/ai" element={<Layout />}>
